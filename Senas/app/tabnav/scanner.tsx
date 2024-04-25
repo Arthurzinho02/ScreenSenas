d26b570d6446a16a747aaea9a8e1ca3c243b82bf
Arthurzinho02 <arthur.matheus0607@gmail.com>
2024-04-25 14:09:27 -0300

    update Camera

--- Senas/app/tabnav/scanner.tsx
+++ Senas/app/tabnav/scanner.tsx
@@ -1,33 +1,60 @@
-import { ScrollView, View, StyleSheet } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, View, Text, SafeAreaView } from 'react-native';
+import { Camera } from 'expo-camera';
 import useColor from '../../temas/Temas';
 
 export default function Scanner() {
-    const color = useColor()
-    const styles = StyleSheet.create({
-        container:{
-          backgroundColor: color.bgPrimary,
-          flex: 1
-        },
-        Cinza:{
-          height: 525,
-          backgroundColor: color.nome === 'dark' ? color.bgPrimary : '#FBFBFB',
-          alignItems: 'center',
-          justifyContent: 'flex-end',
-          paddingBottom: 20
-        }
-      })
+  const color = useColor()
+  //---------------------------------------------------------
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: color.bgPrimary
+    },
+    camera: {
+      width: 450,
+      height: 500,
+    },
+    buttonContainer: {
+      position: 'absolute',
+      bottom: 20,
+      left: 20,
+    },
+    button: {
+      padding: 20,
+      backgroundColor: '#ffffff',
+      borderRadius: 10,
+    },
+    text: {
+      fontSize: 20,
+      color: '#000000',
+    },
+  });
+  //-----------------------------------------------------------------------
+  const camRef = useRef(null);
+  const [hasPermission, setHasPermission] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return <Text>Acesso negado!</Text>;
+  }
+
   return (
-    <>
-    <View style={styles.container}>
-      <ScrollView>
-        <View style={styles.Cinza}>
-        <MaterialIcons name="qr-code-scanner" size={300} color="#5D5C5C" />
-        </View>
-      </ScrollView>
-    </View>
-    </>
+    <SafeAreaView style={styles.container}>
+      <Camera style={styles.camera} ref={camRef}></Camera>
+    </SafeAreaView>
   );
 }
-
 
